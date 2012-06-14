7e12dfe8f4df35b024c44728f68c14ad3c573198
Chon <chon219@gmail.com>
2012-06-15 00:59:20 +0800

    kb category urls

--- moevpn/kb/urls.py
+++ moevpn/kb/urls.py
@@ -1,9 +1,8 @@
-# Chon<chon219@gmail.com>
-
 from django.conf.urls.defaults import patterns, include, url
 from views import *
 
 urlpatterns = patterns('',
   url(r'^$',kb_index),
   url(r'post/(\d+)/$',kb_post),
+  url(r'category/([0-9a-zA-Z_]+)/$',kb_category),
 )
