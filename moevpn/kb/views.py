970c00de95349e01f3f873ee0b0442d4853427d3
Chon <chon219@gmail.com>
2012-06-15 01:50:25 +0800

    kb category views

--- moevpn/kb/views.py
+++ moevpn/kb/views.py
@@ -6,16 +6,35 @@ from django.shortcuts import render_to_response
 from models import *
 
 def kb_index(request):
-  posts = []
-  post_all = Post.objects.all()
-  for post in post_all:
-    posts.append({'post_id':post.post_id,'author':post.user.username,'time':post.post_time,'title':post.title})
-  return render_to_response("kb_index.html",{'posts':posts})
+  posts = Post.objects.all()
+  categories = Category.objects.all()
+  return render_to_response("kb_index.html",{
+      'posts':posts,
+      'categories':categories,
+      'user':request.user})
+
+def kb_category(request,category_name):
+  try:
+      category = Category.objects.get(name=category_name)
+      categories = Category.objects.all()
+      posts = Post.objects.filter(category=category)
+      return render_to_response("kb_index.html",{
+          'posts':posts,
+          'category':category,
+          'categories':categories,
+          'user':request.user})
+  except Exception:
+      return HttpResponseRedirect("/kb/")
 
 def kb_post(request,post_id):
   try:
     p = Post.objects.get(post_id=post_id)
-    post = {'title':p.title,'content':p.content,'time':p.post_time,'author':p.user.username}
-    return render_to_response('kb_post.html',{'post':post})
+    post = {'title':p.title,
+            'content':p.content,
+            'time':p.post_time,
+            'author':p.user.username}
+    return render_to_response('kb_post.html',{
+        'post':post,
+        'user':request.user})
   except Exception:
     return HttpResponseRedirect("/kb/")
